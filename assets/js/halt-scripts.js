959f334746b64352880ef6c0df4e6dd98f75dad5
Ram Ratan Maurya <ratan.maurya@gmail.com>
2014-01-27 20:28:26 +0530

    Improve front-end JS

--- assets/js/halt-scripts.js
+++ assets/js/halt-scripts.js
@@ -1,29 +1,104 @@
-(function ($) {
-    "use strict";
-    $(document).ready(function () {
-        
-        // Article Upvote/Downvote
-        $('[data-article-id]').find('a').on('click', function (e) {
-            e.preventDefault();
-            var container   = $(this).closest('.article-vote'),
-                postid      = parseInt(container.data('article-id')),
-                type        = $(this).data('vote-type'),
-                that        = $(this),
-                data        = {
-                    action: 'halt_article_vote',
-                    nonce: halt_vars.ajax_nonce,
-                    vote_type: type,
-                    postid: postid
-                };
 
-            if (!container.hasClass('voted')) {
-                $.post(halt_vars.ajaxurl, data, function (response) {
-                    that.find('.count').text(response);
-                    container.addClass('voted');
-                });
-            }
-        });
+(function (window, $) {
+    'use strict';
 
-    });
+    // Cache document for fast access.
+	var document = window.document;
 
-}) (jQuery);
+	/**
+	 * The faux "class" constructor.
+	 *
+	 * @since  1.0.
+	 *
+	 * @return void
+	 */
+	var Halt = function () {
+
+		/**
+		 * Holds reusable elements.
+		 *
+		 * @since 1.0.
+		 *
+		 * @type  {{}}
+		 */
+		var cache = {};
+
+		/**
+		 * Initiate all actions for this class.
+		 *
+		 * @since  1.0.
+		 *
+		 * @return void
+		 */
+		function init() {
+			// Cache the reusable elements
+			cacheElements();
+
+			// Bind events
+			bindEvents();
+		}
+
+		/**
+		 * Caches elements that are used in this scope.
+		 *
+		 * @since  1.0.
+		 *
+		 * @return void
+		 */
+		function cacheElements() {
+			cache.$window = $(window);
+			cache.$document = $(document);
+
+			cache.$isSingleArticle = ($('body.single-article').length > 0);
+		}
+
+		/**
+		 * Setup event binding.
+		 *
+		 * @since  1.0.
+		 *
+		 * @return void
+		 */
+		function bindEvents() {
+			if (cache.$isSingleArticle) {
+				cache.$document.on('ready', articleVote);
+			}
+		}
+
+		/**
+		 * Article upvote/downvote functionality.
+		 *
+		 * @since  1.0
+		 *
+		 * @return void
+		 */
+		function articleVote() {
+			$('[data-article-id]').find('a').on('click', function (e) {
+			    e.preventDefault();
+			    var container   = $(this).closest('.article-vote'),
+			        postid      = parseInt(container.data('article-id')),
+			        type        = $(this).data('vote-type'),
+			        that        = $(this),
+			        data        = {
+			            action: 'halt_article_vote',
+			            nonce: halt_vars.ajax_nonce,
+			            vote_type: type,
+			            postid: postid
+			        };
+
+			    if (!container.hasClass('voted')) {
+			        $.post(halt_vars.ajaxurl, data, function (response) {
+			            that.find('.count').text(response);
+			            container.addClass('voted');
+			        });
+			    }
+			});
+		}
+
+		// Initiate the actions.
+		init();
+	};
+
+    // Instantiate the "class".
+    window.Halt = new Halt();
+})(window, jQuery);

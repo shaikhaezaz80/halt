892d81a936dd2a40709f8d4f5fc822dbc9e53320
Ram Ratan Maurya <ratan.maurya@gmail.com>
2014-01-27 19:36:30 +0530

    Merge $_GET and $_POST functions into one

--- includes/actions.php
+++ includes/actions.php
@@ -11,31 +11,18 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Hooks Halt actions, when present in the $_GET superglobal. Every halt_action
- * present in $_GET is called using WordPress's do_action function. These
- * functions are called on init.
+ * Processes all Halt actions sent via POST and GET by looking for the 'halt_action'
+ * request and running do_action() to call the function
  *
- * @since 1.0
  * @return void
 */
-function halt_get_actions() {
+function halt_get_post_actions() {
 	if ( isset( $_GET['halt_action'] ) ) {
 		do_action( 'halt_' . $_GET['halt_action'], $_GET );
 	}
-}
-add_action( 'init', 'halt_get_actions' );
 
-/**
- * Hooks Halt actions, when present in the $_POST superglobal. Every halt_action
- * present in $_POST is called using WordPress's do_action function. These
- * functions are called on init.
- *
- * @since 1.0
- * @return void
-*/
-function halt_post_actions() {
 	if ( isset( $_POST['halt_action'] ) ) {
 		do_action( 'halt_' . $_POST['halt_action'], $_POST );
 	}
 }
-add_action( 'init', 'halt_post_actions' );
+add_action( 'init', 'halt_get_post_actions' );
